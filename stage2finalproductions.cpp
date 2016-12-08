32030beb07e0072ee2f37aaac3329d7b973d7ae4
SayLaVie <mmccarver@angelo.edu>
2016-12-08 17:59:43 -0600

    minor changes

---
+++ stage2finalproductions.cpp
@@ -0,0 +1,27 @@
+#include "stage2.h"
+
+void Stage::IfStmt()
+{
+	//'if' EXPRESS 'then' EXEC_STMT ELSE_PT
+}
+
+void Stage::ElsePt
+{
+	//'else' EXEC_STMT
+	// epsilon
+}
+
+void Stage::WhileStmt()
+{
+	//'while' EXPRESS 'do' EXEC_STMT
+}
+
+void Stage::RepeatStmt()
+{
+	//'repeat' EXEC_STMTS 'until' EXPRESS
+}
+
+void Stage::NullStmt()
+{
+	//';'
+}
